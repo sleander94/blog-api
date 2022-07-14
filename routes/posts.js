198d3790d920037f46bf08782c0d42724e28e39b
Stephen Leander <stephen.leander1@gmail.com>
2022-07-14 14:16:49 -0600

    Add routes for post / posts CRUD operations

--- routes/posts.js
+++ routes/posts.js
@@ -1,24 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const Post = require('../models/post')
 const postController = require('../controllers/postController')
 
 // List of all posts
 router.get('/', postController.posts_get);
-
-router.post('/', (req, res, next) => {
-  let post = new Post({
-    author: 'Bob',
-    timestamp: new Date().toLocaleDateString(),
-    title: 'I\'m cool',
-    text: 'What a neat API',
-    isPublic: false
-  })
-  post.save((err) => {
-    if (err) {
-      return next(err)
-    }
-  })
-})
+router.get('/:id', postController.post_get)
+router.post('/', postController.posts_post)
+router.delete('/:id', postController.post_delete)
+router.put('/:id', postController.post_update)
 
 module.exports = router;
