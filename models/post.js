7239849f84a1d2e702905532605d3d665b67f87e
Stephen Leander <stephen.leander1@gmail.com>
2022-07-15 09:18:07 -0600

    Add authorId field

--- models/post.js
+++ models/post.js
@@ -4,10 +4,11 @@ var Schema = mongoose.Schema;
 
 var PostSchema = new Schema({
   author: { type: String, required: true },
+  authorId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   timestamp: { type: String, required: true },
   title: { type: String, required: true },
   text: { type: String, required: true },
-  isPublic: {type: Boolean, required: true}
+  isPublic: { type: Boolean, required: true },
 });
 
 module.exports = mongoose.model('Post', PostSchema);
