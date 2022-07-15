e83ee267800cb32349d2c902401d663d2bad31ac
Stephen Leander <stephen.leander1@gmail.com>
2022-07-15 08:04:26 -0600

    Remove user router

--- app.js
+++ app.js
@@ -2,18 +2,20 @@ require('dotenv').config();
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+require('./passport');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 const postsRouter = require('./routes/posts');
 
 const app = express();
 
-const mongoDB = process.env.MONGODB_URI || process.env.MONGODB_DEVURI
+const mongoDB = process.env.MONGODB_URI || process.env.MONGODB_DEVURI;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
@@ -21,13 +23,14 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/posts', postsRouter);
+
+// JWT protected routes
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -39,8 +42,6 @@ app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-
   res.status(err.status || 500);
 });
 
