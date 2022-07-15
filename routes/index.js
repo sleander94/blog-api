f148255a03eac28bd24cb131089d2d0ef6546458
Stephen Leander <stephen.leander1@gmail.com>
2022-07-15 15:47:34 -0600

    Add logout post route

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,6 @@ const user_controller = require('../controllers/userController');
 
 router.post('/signup', user_controller.signup_post);
 router.post('/login', user_controller.login_post);
+router.post('/logout', user_controller.logout_post);
 
 module.exports = router;
