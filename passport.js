45f3c776917118b2ebb6ba21e366878c90019d79
Stephen Leander <stephen.leander1@gmail.com>
2022-07-15 08:00:44 -0600

    Initial commit with local & jwt strategies

---
+++ passport.js
@@ -0,0 +1,60 @@
+const passport = require('passport');
+const bcrypt = require('bcryptjs');
+const LocalStrategy = require('passport-local').Strategy;
+const passportJWT = require('passport-jwt');
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
+const jwt = require('jsonwebtoken');
+const User = require('./models/user');
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: 'email',
+      passwordField: 'password',
+    },
+    (email, password, done) => {
+      User.findOne({ email: email }, (err, user) => {
+        if (err) {
+          return done(err);
+        }
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username' });
+        }
+        bcrypt.compare(password, user.password, (err, res) => {
+          if (res) {
+            return done(null, user);
+          } else {
+            return done(null, false, { message: 'Incorrect password' });
+          }
+        });
+      });
+    }
+  )
+);
+
+passport.use(
+  new JWTStrategy(
+    {
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET,
+    },
+    function (jwt_payload, done) {
+      console.log('JWT Running');
+      console.log(jwt_payload.email);
+      User.findOne({ email: jwt_payload.email }, function (err, user) {
+        if (err) {
+          console.log('Err');
+          return done(err, false);
+        }
+        if (user) {
+          console.log('User');
+          return done(null, user);
+        } else {
+          console.log('No User');
+          return done(null, false);
+        }
+      });
+    }
+  )
+);
